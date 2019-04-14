ee8f118b9459d1da2c15bf5926fe18210f94eae3
jeroen.veltmans <jeroen.veltmans@student.kuleuven.be>
2019-04-14 17:28:21 +0200

    don't show the commands on execute

--- runtest.bat
+++ runtest.bat
@@ -1,3 +1,4 @@
+@echo off
 call D:\School\Industrieel_Ingenieur_Master\thesis\Projects\venv\Scripts\activate ^
 call copy D:\School\Industrieel_Ingenieur_Master\thesis\CID19\secrets.yml
 call python manage.py jenkins chat
