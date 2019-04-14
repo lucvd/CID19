33245b5df8a6c8802ff66f8d8618480bc9f4503d
jeroen.veltmans <jeroen.veltmans@student.kuleuven.be>
2019-04-14 17:54:42 +0200

    secrets.yml copied to jenkins but not accessible by anyone who shouldn't see it

--- runtest.bat
+++ runtest.bat
@@ -1,4 +1,5 @@
 @echo off
-call D:\School\Industrieel_Ingenieur_Master\thesis\Projects\venv\Scripts\activate ^
-call copy D:\School\Industrieel_Ingenieur_Master\thesis\CID19\secrets.yml
+call D:\School\Industrieel_Ingenieur_Master\thesis\Projects\venv\Scripts\activate
+call copy %myFile%
 call python manage.py jenkins chat
+call del secrets.yml
