aff7c76e762027dc957e2faab97ab4c3fb97d1aa
jeroen.veltmans <jeroen.veltmans@student.kuleuven.be>
2019-04-14 18:08:10 +0200

    better name for secrets variable

--- runtest.bat
+++ runtest.bat
@@ -1,5 +1,5 @@
 @echo off
 call D:\School\Industrieel_Ingenieur_Master\thesis\Projects\venv\Scripts\activate
-call copy %myFile%
+call copy %secretdjango%
 call python manage.py jenkins chat
 call del secrets.yml
