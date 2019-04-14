1bebd48e148da35c9a28458ded23cf28e34ba0d0
jeroen.veltmans <jeroen.veltmans@student.kuleuven.be>
2019-04-14 17:27:20 +0200

    multiline .bat file by putting call in front

--- runtest.bat
+++ runtest.bat
@@ -1,3 +1,3 @@
-D:\School\Industrieel_Ingenieur_Master\thesis\Projects\venv\Scripts\activate
-copy D:\School\Industrieel_Ingenieur_Master\thesis\CID19\secrets.yml
-python manage.py jenkins chat
+call D:\School\Industrieel_Ingenieur_Master\thesis\Projects\venv\Scripts\activate ^
+call copy D:\School\Industrieel_Ingenieur_Master\thesis\CID19\secrets.yml
+call python manage.py jenkins chat
