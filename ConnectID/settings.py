e6a8cc8ec265ff3ea91dc987e23332ae240c15a0
jeroen.veltmans <jeroen.veltmans@student.kuleuven.be>
2019-04-14 21:33:53 +0200

    pylint import

--- ConnectID/settings.py
+++ ConnectID/settings.py
@@ -219,6 +219,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 JENKINS_TASKS = (
     'django_jenkins.tasks.run_pep8',
     'django_jenkins.tasks.run_pyflakes',
+    'django_jenkins.tasks.run_pylint',
  #   'django_jenkins.tasks.run_jslint',
  #   'django_jenkins.tasks.run_csslint',
  #   'django_jenkins.tasks.run_sloccount'
--- chat/tests/test_jenkins_delete_later.py
+++ chat/tests/test_jenkins_delete_later.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 
+
 class jenkinsunittest(TestCase):
     def test_alwaystrue(self):
         self.assertEqual('foo'.upper(), 'FOO')
