d45cb44289c61971602e0988ffc8f6b5a7e6c695
jeroen.veltmans <jeroen.veltmans@student.kuleuven.be>
2019-05-08 18:28:34 +0200

    updated runtest for jenkins

--- runtest.bat
+++ runtest.bat
@@ -1,5 +1,5 @@
 @echo off
 call D:\School\Industrieel_Ingenieur_Master\thesis\Projects\venv\Scripts\activate
 call copy %secretsdjango%
-call python manage.py jenkins chat
+call python manage.py jenkins chat --enable-coverage
 call del secrets.yml
