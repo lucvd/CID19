0d464e663607fdaf5f0d118312bf22f104181912
jeroen.veltmans <jeroen.veltmans@student.kuleuven.be>
2019-05-21 12:04:17 +0200

    make it better3

--- ConnectID/settings.py
+++ ConnectID/settings.py
@@ -73,6 +73,7 @@ INSTALLED_APPS = [
 TELLME_FEEDBACK_EMAIL = EMAIL_HOST_USER
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -248,3 +249,26 @@ JENKINS_TASKS = (
  #   'django_jenkins.tasks.run_csslint',
  #   'django_jenkins.tasks.run_sloccount'
 )
+
+
+#TODO below is for heroku together with the whitenoise line in middleware
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.11/howto/static-files/
+PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
+STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra lookup directories for collectstatic to find static files
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+#  Add configuration for static files storage using whitenoise
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+
+import dj_database_url
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
