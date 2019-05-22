9e67878c47facc65363fb8db751db6d4f8b7dd2e
jeroen.veltmans <jeroen.veltmans@student.kuleuven.be>
2019-05-22 19:08:59 +0200

    updated rundeploy

--- runDeploy.bat
+++ runDeploy.bat
@@ -1,4 +1,5 @@
 call heroku git:remote -a connect-id-test
+call git checkout -b local_branch_for_heroku
 call git add secrets.yml
 call git commit -m "commit with secrets"
-call git push heroku 92b69e98:master --force
+call git push heroku local_branch_for_heroku:master --force
